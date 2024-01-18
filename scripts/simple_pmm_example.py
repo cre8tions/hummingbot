e3e2158e0603f422013242e4ccc60d9a5a2e325f
Joe Kushner <joe@cre8tions.net>
2024-01-17 20:10:31 -0800

    testingh

--- scripts/simple_pmm_example.py
+++ scripts/simple_pmm_example.py
@@ -18,13 +18,14 @@ class SimplePMM(ScriptStrategyBase):
     exchange, with a distance defined by the ask_spread and bid_spread. Every order_refresh_time in seconds,
     the bot will cancel and replace the orders.
     """
+
     bid_spread = 0.001
-    ask_spread = 0.001
-    order_refresh_time = 15
-    order_amount = 0.01
+    ask_spread = 0.01
+    order_refresh_time = 120
+    order_amount = 100
     create_timestamp = 0
-    trading_pair = "ETH-USDT"
-    exchange = "kucoin_paper_trade"
+    trading_pair = "FSC-USDT"
+    exchange = "xeggex"
     # Here you can use for example the LastTrade price to use in your strategy
     price_source = PriceType.MidPrice
 
@@ -39,15 +40,30 @@ class SimplePMM(ScriptStrategyBase):
             self.create_timestamp = self.order_refresh_time + self.current_timestamp
 
     def create_proposal(self) -> List[OrderCandidate]:
-        ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
+        ref_price = self.connectors[self.exchange].get_price_by_type(
+            self.trading_pair,
+            self.price_source
+        )
         buy_price = ref_price * Decimal(1 - self.bid_spread)
         sell_price = ref_price * Decimal(1 + self.ask_spread)
 
-        buy_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                   order_side=TradeType.BUY, amount=Decimal(self.order_amount), price=buy_price)
+        buy_order = OrderCandidate(
+            trading_pair=self.trading_pair,
+            is_maker=True,
+            order_type=OrderType.LIMIT,
+            order_side=TradeType.BUY,
+            amount=Decimal(self.order_amount),
+            price=buy_price,
+        )
 
-        sell_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                    order_side=TradeType.SELL, amount=Decimal(self.order_amount), price=sell_price)
+        sell_order = OrderCandidate(
+            trading_pair=self.trading_pair,
+            is_maker=True,
+            order_type=OrderType.LIMIT,
+            order_side=TradeType.SELL,
+            amount=Decimal(self.order_amount),
+            price=sell_price,
+        )
 
         return [buy_order, sell_order]
 
@@ -61,17 +77,27 @@ class SimplePMM(ScriptStrategyBase):
 
     def place_order(self, connector_name: str, order: OrderCandidate):
         if order.order_side == TradeType.SELL:
-            self.sell(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
-                      order_type=order.order_type, price=order.price)
+            self.sell(
+                connector_name=connector_name,
+                trading_pair=order.trading_pair,
+                amount=order.amount,
+                order_type=order.order_type,
+                price=order.price,
+            )
         elif order.order_side == TradeType.BUY:
-            self.buy(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
-                     order_type=order.order_type, price=order.price)
+            self.buy(
+                connector_name=connector_name,
+                trading_pair=order.trading_pair,
+                amount=order.amount,
+                order_type=order.order_type,
+                price=order.price,
+            )
 
     def cancel_all_orders(self):
         for order in self.get_active_orders(connector_name=self.exchange):
             self.cancel(self.exchange, order.trading_pair, order.client_order_id)
 
     def did_fill_order(self, event: OrderFilledEvent):
-        msg = (f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}")
+        msg = f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}"
         self.log_with_clock(logging.INFO, msg)
         self.notify_hb_app_with_timestamp(msg)
--- scripts/v2_market-making_dman_composed.py
+++ scripts/v2_market-making_dman_composed.py
@@ -18,77 +18,154 @@ from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
 class MarketMakingDmanComposed(ScriptStrategyBase):
-    trading_pair = "HBAR-USDT"
+    trading_pair = "FSC-USDT"
     triple_barrier_conf_top = TripleBarrierConf(
-        stop_loss=Decimal("0.03"), take_profit=Decimal("0.02"),
+        stop_loss=Decimal("0.03"),
+        take_profit=Decimal("0.02"),
         time_limit=60 * 60 * 1,
         trailing_stop_activation_price_delta=Decimal("0.002"),
-        trailing_stop_trailing_delta=Decimal("0.0005")
+        trailing_stop_trailing_delta=Decimal("0.0005"),
     )
     triple_barrier_conf_bottom = TripleBarrierConf(
-        stop_loss=Decimal("0.03"), take_profit=Decimal("0.02"),
+        stop_loss=Decimal("0.03"),
+        take_profit=Decimal("0.02"),
         time_limit=60 * 60 * 3,
         trailing_stop_activation_price_delta=Decimal("0.005"),
-        trailing_stop_trailing_delta=Decimal("0.001")
+        trailing_stop_trailing_delta=Decimal("0.001"),
     )
 
     config_v1 = DManV1Config(
-        exchange="binance_perpetual",
+        exchange="xeggex",
         trading_pair=trading_pair,
         order_levels=[
-            OrderLevel(level=0, side=TradeType.BUY, order_amount_usd=Decimal("15"),
-                       spread_factor=Decimal(1.0), order_refresh_time=60 * 30,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_top),
-            OrderLevel(level=1, side=TradeType.BUY, order_amount_usd=Decimal("50"),
-                       spread_factor=Decimal(5.0), order_refresh_time=60 * 30,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=2, side=TradeType.BUY, order_amount_usd=Decimal("50"),
-                       spread_factor=Decimal(8.0), order_refresh_time=60 * 15,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=0, side=TradeType.SELL, order_amount_usd=Decimal("15"),
-                       spread_factor=Decimal(1.0), order_refresh_time=60 * 30,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_top),
-            OrderLevel(level=1, side=TradeType.SELL, order_amount_usd=Decimal("50"),
-                       spread_factor=Decimal(5.0), order_refresh_time=60 * 30,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=2, side=TradeType.SELL, order_amount_usd=Decimal("50"),
-                       spread_factor=Decimal(8.0), order_refresh_time=60 * 15,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
+            OrderLevel(
+                level=0,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal("15"),
+                spread_factor=Decimal(1.0),
+                order_refresh_time=60 * 30,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_top,
+            ),
+            OrderLevel(
+                level=1,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal("50"),
+                spread_factor=Decimal(5.0),
+                order_refresh_time=60 * 30,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=2,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal("50"),
+                spread_factor=Decimal(8.0),
+                order_refresh_time=60 * 15,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=0,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal("15"),
+                spread_factor=Decimal(1.0),
+                order_refresh_time=60 * 30,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_top,
+            ),
+            OrderLevel(
+                level=1,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal("50"),
+                spread_factor=Decimal(5.0),
+                order_refresh_time=60 * 30,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=2,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal("50"),
+                spread_factor=Decimal(8.0),
+                order_refresh_time=60 * 15,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
         ],
         candles_config=[
-            CandlesConfig(connector="binance_perpetual", trading_pair=trading_pair, interval="3m", max_records=1000),
+            CandlesConfig(connector="xeggex", trading_pair=trading_pair, interval="3m", max_records=1000),
         ],
         leverage=25,
-        natr_length=21
+        natr_length=21,
     )
     config_v2 = DManV2Config(
-        exchange="binance_perpetual",
+        exchange="xeggex",
         trading_pair=trading_pair,
         order_levels=[
-            OrderLevel(level=0, side=TradeType.BUY, order_amount_usd=Decimal(15),
-                       spread_factor=Decimal(1.0), order_refresh_time=60 * 5,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_top),
-            OrderLevel(level=1, side=TradeType.BUY, order_amount_usd=Decimal(30),
-                       spread_factor=Decimal(2.0), order_refresh_time=60 * 5,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=2, side=TradeType.BUY, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(3.0), order_refresh_time=60 * 15,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=0, side=TradeType.SELL, order_amount_usd=Decimal(15),
-                       spread_factor=Decimal(1.0), order_refresh_time=60 * 5,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_top),
-            OrderLevel(level=1, side=TradeType.SELL, order_amount_usd=Decimal(30),
-                       spread_factor=Decimal(2.0), order_refresh_time=60 * 5,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
-            OrderLevel(level=2, side=TradeType.SELL, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(3.0), order_refresh_time=60 * 15,
-                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf_bottom),
+            OrderLevel(
+                level=0,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal(15),
+                spread_factor=Decimal(1.0),
+                order_refresh_time=60 * 5,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_top,
+            ),
+            OrderLevel(
+                level=1,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal(30),
+                spread_factor=Decimal(2.0),
+                order_refresh_time=60 * 5,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=2,
+                side=TradeType.BUY,
+                order_amount_usd=Decimal(50),
+                spread_factor=Decimal(3.0),
+                order_refresh_time=60 * 15,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=0,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal(15),
+                spread_factor=Decimal(1.0),
+                order_refresh_time=60 * 5,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_top,
+            ),
+            OrderLevel(
+                level=1,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal(30),
+                spread_factor=Decimal(2.0),
+                order_refresh_time=60 * 5,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
+            OrderLevel(
+                level=2,
+                side=TradeType.SELL,
+                order_amount_usd=Decimal(50),
+                spread_factor=Decimal(3.0),
+                order_refresh_time=60 * 15,
+                cooldown_time=15,
+                triple_barrier_conf=triple_barrier_conf_bottom,
+            ),
         ],
         candles_config=[
-            CandlesConfig(connector="binance_perpetual", trading_pair=trading_pair, interval="3m", max_records=1000),
+            CandlesConfig(connector="xeggex", trading_pair=trading_pair, interval="3m", max_records=1000),
         ],
         leverage=25,
-        natr_length=21, macd_fast=12, macd_slow=26, macd_signal=9
+        natr_length=21,
+        macd_fast=12,
+        macd_slow=26,
+        macd_signal=9,
     )
     dman_v1 = DManV1(config=config_v1)
     dman_v2 = DManV2(config=config_v2)
@@ -130,16 +207,20 @@ class MarketMakingDmanComposed(ScriptStrategyBase):
             for trading_pair, position in connector.account_positions.items():
                 if trading_pair in self.markets[connector_name]:
                     if position.position_side == PositionSide.LONG:
-                        self.sell(connector_name=connector_name,
-                                  trading_pair=position.trading_pair,
-                                  amount=abs(position.amount),
-                                  order_type=OrderType.MARKET,
-                                  price=connector.get_mid_price(position.trading_pair),
-                                  position_action=PositionAction.CLOSE)
+                        self.sell(
+                            connector_name=connector_name,
+                            trading_pair=position.trading_pair,
+                            amount=abs(position.amount),
+                            order_type=OrderType.MARKET,
+                            price=connector.get_mid_price(position.trading_pair),
+                            position_action=PositionAction.CLOSE,
+                        )
                     elif position.position_side == PositionSide.SHORT:
-                        self.buy(connector_name=connector_name,
-                                 trading_pair=position.trading_pair,
-                                 amount=abs(position.amount),
-                                 order_type=OrderType.MARKET,
-                                 price=connector.get_mid_price(position.trading_pair),
-                                 position_action=PositionAction.CLOSE)
+                        self.buy(
+                            connector_name=connector_name,
+                            trading_pair=position.trading_pair,
+                            amount=abs(position.amount),
+                            order_type=OrderType.MARKET,
+                            price=connector.get_mid_price(position.trading_pair),
+                            position_action=PositionAction.CLOSE,
+                        )
